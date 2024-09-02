e3ad4ee73a73768ae57b3d2210a0f7f74aae5a65
Jay Freeman (saurik) <saurik@saurik.com>
2024-09-02 15:42:59 -0700

    Upgrade to the r27 NDK and fix a ton of new lints.

--- env/setup-ndk.sh
+++ env/setup-ndk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 set -o pipefail
-echo y | "${ANDROID_HOME}"/cmdline-tools/latest/bin/sdkmanager "build-tools;29.0.2" "ndk;26.3.11579264" "platforms;android-33" >/dev/null
+echo y | "${ANDROID_HOME}"/cmdline-tools/latest/bin/sdkmanager "build-tools;29.0.2" "ndk;27.0.12077973" "platforms;android-33" >/dev/null
