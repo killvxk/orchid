c576b7a28440a48f0f3f127e736b7df00c9af96d
Jay Freeman (saurik) <saurik@saurik.com>
2024-09-02 23:57:52 -0700

    Finish upgrading to the latest version of Flutter.

--- env/setup-ndk.sh
+++ env/setup-ndk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 set -o pipefail
-echo y | "${ANDROID_HOME}"/cmdline-tools/latest/bin/sdkmanager "build-tools;29.0.2" "ndk;27.0.12077973" "platforms;android-33" >/dev/null
+echo y | "${ANDROID_HOME}"/cmdline-tools/latest/bin/sdkmanager \
+    "build-tools;30.0.3" \
+    "ndk;27.0.12077973" \
+    "platforms;android-34" \
+>/dev/null
