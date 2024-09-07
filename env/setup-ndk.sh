85d1cc858f98151ed1967b1edddf0bdebcd523bb
Jay Freeman (saurik) <saurik@saurik.com>
2024-09-06 19:22:31 -0700

    Try to fix reproduction of server build.

--- env/setup-ndk.sh
+++ env/setup-ndk.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 set -o pipefail
+
+ndk=27.0.12077973
+
 echo y | "${ANDROID_HOME}"/cmdline-tools/latest/bin/sdkmanager \
     "build-tools;30.0.3" \
-    "ndk;27.0.12077973" \
+    "ndk;${ndk}" \
     "platforms;android-34" \
 >/dev/null
+
+export ANDROID_NDK_ROOT="${ANDROID_HOME}/ndk/${ndk}"
+
+if [[ -n ${GITHUB_ENV} ]]; then
+    echo "ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT}" >>"${GITHUB_ENV}"
+fi
